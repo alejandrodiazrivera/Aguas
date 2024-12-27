ce01fdab77ae54cf1e736792412931e31e2d19f3
alejandrodiazrivera <alejandrodiazrivera@outlook.com>
2024-12-28 00:17:39 +0100

    life is life

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// filepath: /c:/Users/Cynthia Rivera/OneDrive/Desktop/Aguas VVS/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/aguas-vvs/', // Replace 'aguas-vvs' with your repository name
+  plugins: [react()]
 })
