0bf8cb0fd9c6b2f03c248db55ddb017fe531b71f
alejandrodiazrivera <alejandrodiazrivera@outlook.com>
2024-12-30 16:14:47 +0100

    dk

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -11,11 +11,11 @@ const About = ({ setPlayState }) => {
             <img src={play_icon} alt='' className='play_icon' onClick={() => { setPlayState(true) }} />
         </div>
         <div className='about-right'>
-            <h3>About AguaS Plumbing</h3>
-            <h2>Providing the best plumbing services in town</h2>
-            <p>At Aguas, we take pride in being your trusted partner for all your plumbing needs. With a commitment to delivering top-notch services, we combine expertise, reliability, and a customer-first approach to ensure your satisfaction every time.</p>
-            <p>Our skilled team of professionals is dedicated to providing timely solutions, whether it’s a quick fix or a complex installation. We understand that plumbing problems can disrupt your daily life, which is why we prioritize efficient and high-quality workmanship that stands the test of time. We don’t just fix pipes – we build trust. Our reputation as the best plumbing service in town is founded on our core values: integrity, transparency, and a genuine passion for helping our community.</p>
-            <p>Let us handle your plumbing challenges so you can enjoy peace of mind knowing your home or business is in the best hands.</p>
+            <h3>Om Aguas VVS</h3>
+            <h2>Leverer de bedste VVS-tjenester i byen</h2>
+            <p>Hos Aguas er vi stolte af at være din betroede partner til alle dine VVS-behov. Med et stærkt engagement i at levere førsteklasses service kombinerer vi ekspertise, pålidelighed og en kunde-først tilgang for at sikre din tilfredshed hver gang.</p>
+            <p>Vores dygtige team af professionelle er dedikeret til at levere rettidige løsninger, uanset om det drejer sig om en hurtig reparation eller en kompleks installation. Vi forstår, at VVS-problemer kan forstyrre din hverdag, og derfor prioriterer vi effektivt og kvalitetsbevidst håndværk, der holder i længden. Vi reparerer ikke bare rør – vi opbygger tillid. Vores ry som den bedste VVS-service i byen bygger på vores kerneværdier: integritet, gennemsigtighed og en ægte passion for at hjælpe vores lokalsamfund.</p>
+            <p>Lad os tage os af dine VVS-udfordringer, så du kan nyde ro i sindet og vide, at dit hjem eller din virksomhed er i de bedste hænder.</p>
         </div>
     </div>
   )
